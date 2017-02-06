884808a1a1e5744a9b055bd46ad6fca3ad022f60
saral1230 <xuying.liu@gmail.com>
2017-02-06 15:45:13 -0500

    Delete loadpowerdata.R

--- loadpowerdata.R
+++
@@ -1,7 +0,0 @@
-  filename <- "household_power_consumption.txt"
-  power <- read.table(filename, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?")
-  # convert date and time variables to Date/Time class
-  power$Time <- strptime(paste(power$Date, power$Time), "%d/%m/%Y %H:%m:%s")
-  power$Date <- as.Date(power$Date, "%d/%m/%Y")
-  # only use data from the dates 2007-02-01 and 2007-02-02
-  dfp<-power[power$Date =="2007-02-01"| power$Date =="2007-02-02",]
