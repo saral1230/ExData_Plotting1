fd730b0d3fa38f4fa427c20b4587302f39b6020f
saral1230 <xuying.liu@gmail.com>
2017-02-07 11:44:05 -0500

    Update loadpowerdata.R

--- scripts/loadpowerdata.R
+++ scripts/loadpowerdata.R
@@ -1,7 +1,10 @@
   filename <- "household_power_consumption.txt"
   power <- read.table(filename, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?")
-  # convert date and time variables to Date/Time class
-  power$Time <- strptime(paste(power$Date, power$Time), "%d/%m/%Y %H:%m:%s")
+
+  # convert date variables to Date/Time class
   power$Date <- as.Date(power$Date, "%d/%m/%Y")
+
   # only use data from the dates 2007-02-01 and 2007-02-02
   df<-power[power$Date =="2007-02-01"| power$Date =="2007-02-02",]
+  datetime <- paste(df$Date, df$Time)
+  df$Datetime <- as.POSIXct(datetime)
