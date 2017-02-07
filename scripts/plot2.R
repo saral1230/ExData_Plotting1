641b99a58c057701d3f90d08f24efd5bec847fd9
saral1230 <xuying.liu@gmail.com>
2017-02-07 11:47:45 -0500

    Update plot2.R

--- scripts/plot2.R
+++ scripts/plot2.R
@@ -1,5 +1,5 @@
 plot2<-function(){
-  plot(df$datetime, df$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+  plot(df$Datetime, df$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
   dev.copy(png, file="plot2.png", width=480, height=480)
   dev.off()
   cat("plot2.png has been saved in ", getwd())
