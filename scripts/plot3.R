f4e55d3934b918cbd55ab1a8f7b47d36717388dd
saral1230 <xuying.liu@gmail.com>
2017-02-07 12:06:04 -0500

    Update plot3.R

--- scripts/plot3.R
+++ scripts/plot3.R
@@ -1,8 +1,10 @@
 plot3<-function(){
+  plot(df$Datetime, df$Sub_metering_1, type = "l", xlab = "", ylab = "Energy_Sub_metering")
+  lines(df$Datetime, df$Sub_metering_2, col="red")
+  lines(df$Datetime, df$Sub_metering_3, col="blue")
   legend("topright", col = c("black", "red", "blue"), lty=1, lwd=2, 
          legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") )
   dev.copy(png, file="plot3.png", width=480, height=480)
   dev.off()
-  cat("plot2.png has been saved in ", getwd())
-  
-}
+  cat("plot3.png has been saved in ", getwd())
+  }
